21593423f92e22a21a4352a697bddfabf98d492d
BraydenFou <6194481@johnabbottcollege.net>
2024-12-03 00:01:56 -0500

    Fixed small bugs.

--- ECommerce.Api.Search/Services/SearchService.cs
+++ ECommerce.Api.Search/Services/SearchService.cs
@@ -7,41 +7,45 @@ namespace ECommerce.Api.Search.Services
 {
     public class SearchService : ISearchService
     {
-        
-        private readonly IOrdersService ordersService;
         private readonly IProductsService productsService;
+        private readonly IOrdersService ordersService;
+        private readonly ICustomersService customersService;
 
-        public SearchService(IOrdersService ordersService, IProductsService productsService)
+        public SearchService(IProductsService productsService,
+            IOrdersService ordersService, ICustomersService customersService)
         {
-            this.ordersService = ordersService;
             this.productsService = productsService;
+            this.ordersService = ordersService;
+            this.customersService = customersService;
         }
         public async Task<(bool IsSuccess, dynamic SearchResults)> SearchAsync(int customerId)
         {
-            var ordersResults = await ordersService.GetOrdersAsync(customerId);
+            var customersResult = await customersService.GetCustomerAsync(customerId);
+            var ordersResult = await ordersService.GetOrdersAsync(customerId);
+            var productsResult = await productsService.GetProductsAsync();
 
-            var productResults = await productsService.GetProductsAsync();
-
-            if(ordersResults.IsSuccess)
+            if (ordersResult.IsSuccess)
             {
-                foreach (var order in ordersResults.Orders)
+                foreach (var orders in ordersResult.Orders)
                 {
-                    foreach(var item in order.Items)
+                    foreach (var item in orders.Items)
                     {
-                        item.ProductName = productResults.IsSuccess ?
-                            productResults.Products.FirstOrDefault(p => p.Id == item.ProductId)?.Name :
+                        item.ProductName = productsResult.IsSuccess ?
+                            productsResult.Products.FirstOrDefault(p => p.Id == item.ProductId)?.Name :
                             "Product information is not available";
                     }
                 }
                 var result = new
                 {
-                    Orders = ordersResults.Orders
+                    Customer = customersResult.IsSuccess ?
+                                customersResult.Customer :
+                                new { Name = "Customer information is not available" },
+                    Orders = ordersResult.Orders
                 };
 
-                return(true, result);
+                return (true, result);
             }
-
-            return(false, null);
+            return (false, null);
         }
     }
 }
